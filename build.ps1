aec3bc884e80d70fd4c75deb1226db87c902df5d
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2026-01-12 19:29:56 +0700

    Update

--- build.ps1
+++ build.ps1
@@ -5,11 +5,3 @@ npm run pdf $folder
 $folder = "src/T02"
 npm run html $folder
 npm run pdf $folder
-
-# $folder = "src/T03"
-# npm run html $folder
-# npm run pdf $folder
-
-# $folder = "src/T04"
-# npm run html $folder
-# npm run pdf $folder
